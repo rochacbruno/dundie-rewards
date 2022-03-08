765056dc72b431325938a7e09eec32123bfe9da7
Bruno Rocha <rochacbruno@users.noreply.github.com>
2022-03-08 18:11:10 +0000

    Added people file closes #9 (#12)
    closes #9

--- dundie/__init__.py
+++
@@ -1,1 +0,0 @@
-print("hello initializing dundie...")
--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,4 +1,39 @@
+import argparse
+
+
+def load(filepath):
+    """Loads data from filepath to the database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
+
 
 def main():
-    print("Executing dundie from entryu point.")
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and use with cautious.",
+    )   
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load", "show", "send"),
+        default="help",
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None,
+    )
+    args = parser.parse_args()
+
     
+    globals()[args.subcommand](args.filepath)    
+    
+
+if __name__ == "__main__":
+    main()
