7966e53554ad665417f842fe61a015d8cc9b6250
Bruno Rocha <rochacbruno@gmail.com>
2022-03-11 12:44:37 +0000

    parametrize e testes negativos

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,12 +1,25 @@
 import pytest
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_positive_call_load_command():
     """test command load"""
     out = check_output(
         ["dundie", "load", "tests/assets/people.csv"]
     ).decode("utf-8").split("\n")
     assert len(out) == 2
+
+
+@pytest.mark.integration
+@pytest.mark.medium
+@pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
+def test_load_negative_call_load_command_with_wrong_params(wrong_command):
+    """test command load"""
+    with pytest.raises(CalledProcessError) as error:
+        check_output(
+            ["dundie", wrong_command, "tests/assets/people.csv"]
+        ).decode("utf-8").split("\n")
+
+    assert "status 2" in str(error.getrepr())
