f20729969ebde3523d1a2d1c497243876bc848c2
Bruno Rocha <rochacbruno@gmail.com>
2022-04-12 13:07:07 +0100

    Migramos o legado de json para SQL com SQLModel

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
+import warnings
 import pytest
 from unittest.mock import patch
+from sqlmodel import create_engine
+from dundie import models
+from sqlalchemy.exc import SAWarning
+
+
+warnings.filterwarnings("ignore", category=SAWarning)
 
 
 MARKER = """\
@@ -29,6 +36,8 @@ def setup_testing_database(request):
     force database.py to use that filepath.
     """
     tmpdir = request.getfixturevalue("tmpdir")
-    test_db = str(tmpdir.join("database.test.json"))
-    with patch("dundie.database.DATABASE_PATH", test_db):
+    test_db = str(tmpdir.join("database.test.db"))
+    engine = create_engine(f"sqlite:///{test_db}")
+    models.SQLModel.metadata.create_all(bind=engine)
+    with patch("dundie.database.engine", engine):
         yield
