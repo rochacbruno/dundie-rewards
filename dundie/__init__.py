31263a9b2e70afa44862811d0f9f8df2928a0cf0
Vinicius Telles <figueiredovt@yahoo.com.br>
2022-07-20 17:49:25 -0300

    create  requirements file, Markfile and people.csv and update setup.

--- dundie/__init__.py
+++ dundie/__init__.py
@@ -1,1 +1,1 @@
-print("Hello initializing dundie....")
+
--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,7 +1,43 @@
 
+import argparse
+
+def load(filepath):
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
+
+
+
 def main():
-    print("Executing entry point for dundie...")
+    parser = argparse.ArgumentParser(
+    description="Dunder Mifflin Rewards CLI",
+    epilog="Enjoy and use with cautions",
+)
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load", "show", "send"),
+        default=help
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None
+    )
 
 
-if __name__ == '__main__':
+    args = parser.parse_args()
+
+    try:
+       globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid.")
+
+
+if __name__ == "__main__":
     main()
