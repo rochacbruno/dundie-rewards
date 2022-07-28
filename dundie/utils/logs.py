bd2ccad25275ac62e0bbdcf752ce737a1f90e310
Vinicius Telles <figueiredovt@yahoo.com.br>
2022-07-28 08:34:17 -0300

    teste 28 julho

--- dundie/utils/logs.py
+++ dundie/utils/logs.py
@@ -1,21 +1,30 @@
-import os
 import logging
+import os
 from logging import handlers
+from typing import Union
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.getLogger("dundie")
 fmt = logging.Formatter(
-        '%(asctime)s %(name)s %(levelname)s'
-        'l:%(lineno)d f:%(filename)s: %(message)s'
+    "%(asctime)s  %(name)s  %(levelname)s "
+    "l:%(lineno)d f:%(filename)s: %(message)s"
 )
 
-def get_logger(logfile="dundie.log"):
 
+def get_logger(
+    logfile: Union[str, os.PathLike[str]] = "dundie.log"
+) -> logging.Logger:
+    """Returns a configured logger."""
+    # ch = logging.StreamHandler()  # Console/terminal/stderr
+    # ch.setLevel(log_level)
     fh = handlers.RotatingFileHandler(
         logfile,
-        maxBytes=300,
-        backupCount=10
+        maxBytes=10**6,
+        backupCount=10,
     )
     fh.setLevel(LOG_LEVEL)
+    # ch.setFormatter(fmt)
     fh.setFormatter(fmt)
+    # log.addHandler(ch)
     log.addHandler(fh)
+    return log
