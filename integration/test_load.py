09e70ffe4a8a5a75faf53c4c4e46d9fb364a8019
Vinicius Telles <figueiredovt@yahoo.com.br>
2022-07-30 12:20:19 -0300

    teste 9 30 julho

--- integration/test_load.py
+++ integration/test_load.py
@@ -2,12 +2,10 @@ import pytest
 from subprocess import check_output
 
 
-
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load():
     """test command load"""
-
     out = check_output(
         ["dundie", "load", "tests/assets/people.csv"]
     ).decode("utf-8").split("|")
